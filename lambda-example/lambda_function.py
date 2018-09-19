a66f31dd7b467ca5fc93580ba6da066e20bd9d03
Kyle Polich <kyle@dataskeptic.com>
2018-09-19 12:10:10 -0400

    updates

--- lambda-example/lambda_function.py
+++ lambda-example/lambda_function.py
@@ -5,6 +5,12 @@ import json
 import random
 import pandas as pd
 from datetime import datetime
+import pickle
+import sklearn
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+from sklearn.datasets import load_boston
 
 for d, _, files in os.walk('lib'):
     for f in files:
@@ -14,7 +20,9 @@ for d, _, files in os.walk('lib'):
 
 import numpy as np
 
-s3r = boto3.resource('s3')
+s3_bucket = os.getenv('S3_BUCKET')
+
+s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
 	print('========[START]========================================')
@@ -31,28 +39,41 @@ def lambda_handler(event, context):
 	#influxdb_user = os.environ['user']
 	#influxdb_password = os.environ['password']
 	#db_influx = InfluxDBClient(host=host, port=port, username=user, password=password, database=database, ssl=True)
-	print("--------[Getting data]---------------------------------")
+	print("--------[Getting model]--------------------------------")
 	bucket = event['bucket']
-	s3key = event['s3key']	
-	obj = s3r.Object(bucket, s3key)
-	s = obj.get()['Body'].read().decode('utf-8') 
-	resp = json.loads(s)
+	s3key = event['s3key']
+	observation = event['observation']
+	fname = 'latest.pickle'
+	s3.Bucket(bucket).download_file(s3key, fname)
+	model = pickle.load(open(fname, 'rb'))
+	obs = pd.DataFrame([observation])
+	prediction = model.predict(obs)
 	print("--------[Classification]-------------------------------")
 
 	resp = {
-		"label": label,
-		"version": "v1.0.0", 
-		"bucket": bucket,
-		"s3key": s3key
+		"label": prediction,
+		"version": "v1.0.0"
 	}
 	print("--------[Persist result]-------------------------------")
 	# Save here or let the caller handle it?
 	return resp
 
 
-#s3key = 'stub/sample.vega'
+test_obj = {'CRIM': 0.00632,
+ 'ZN': 18.0,
+ 'INDUS': 2.31,
+ 'CHAS': 0.0,
+ 'NOX': 0.538,
+ 'RM': 6.575,
+ 'AGE': 65.2,
+ 'DIS': 4.09,
+ 'RAD': 1.0,
+ 'TAX': 296.0,
+ 'PTRATIO': 15.3,
+ 'B': 396.9,
+ 'LSTAT': 4.98}
 
-event = {"bucket": "blah", "s3key": "test"}
+event = {"bucket": "ml-in-prod", "s3key": "models/latest.pickle", "observation": test_obj}
 
 context = {}
 result = lambda_handler(event, context)
--- lambda-example/test_local.sh
+++ lambda-example/test_local.sh
@@ -1,6 +1,6 @@
-export host=x
-export database=y
-export port=z
-export user=a
-export password=b
+export USER=$(cat creds.txt | awk '{print $1}')
+export PASSWORD=$(cat creds.txt | awk '{print $2}')
+export AWS_ACCESS_KEY=$(cat creds.txt | awk '{print $3}')
+export AWS_SECRET_KEY=$(cat creds.txt | awk '{print $4}')
+export S3_BUCKET=$(cat creds.txt | awk '{print $5}')
 python3 lambda_function.py
